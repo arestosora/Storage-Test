05c707e34bdac472da88e85a5bf03be0391905b7
A. Mestra (Rain) <adonymestra2@gmail.com>
2023-10-20 02:00:02 -0500

    Modules

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
+import { UsersController } from '../users/users.controller';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { DatabaseConfig } from './services/database/database.config';
-import { User } from './users/user.entity';
+import { DatabaseConfig } from '../services/database/database.config';
+import { User } from '../users/user.entity';
 
 @Module({
   imports: [
